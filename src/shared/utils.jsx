32ed46604a07cb0fb178316d27f817d56f84a56b
Benjamin Faleye <benjamin.faleye@wemabank.com>
2019-05-16 17:19:34 +0100

    [Benjamin] Properly handled Empty customer account list situation on dashboard - Fixed onboarding Error where skip BVN after failed BVN verifiation still allows doc upload. -Fixed proper handling of onboarding error

--- src/shared/utils.jsx
+++ src/shared/utils.jsx
@@ -18,38 +18,26 @@ export const canvasToFile = (dataURL) =>{
   };
 
 export const modelStateErrorHandler = (error, field) => {
-   if ("modelState" in error && error.message.toLowerCase().indexOf('the request is invalid.')>-1){
-       //This is a model state error, process each model state object
-    //    console.log('error is ', error);
-    //    for(let err of error.modelState){
-    //        console.log('each error is',err);
-    //        return err.field.join();
-    //        // for(let errMsg of err.field){
-    //        //     var stringValueYouWant = a.join()
-    //        //     return (errMsg)
-    //        // }
-    //    }
+    console.log("in model state");
+  if(error.response) {
+    if ("modelState" in error.response.data && error.response.data.message.toLowerCase().indexOf('the request is invalid.')>-1){
         let message = '';
-       for(let key in error.modelState){
-            if (error.modelState.hasOwnProperty(key)) {
+       for(let key in error.response.data.modelState){
+            if ( error.response.data.modelState.hasOwnProperty(key)) {
                 // console.log(key + " -> " + error.modelState[key]);
-                if(Object.keys(error.modelState).length>1){
-                    message+=error.modelState[key]+', ';
+                if(Object.keys(error.response.data.modelState).length>1){
+                    message+=error.response.data.modelState[key]+', ';
                 }else{
-                    message+=error.modelState[key];
+                    message+=error.response.data.modelState[key];
                 }
-                
             }
        }
        return message;
-
-   }
-   else{
-    //    console.log('here');
-    //   return error.message || error.Message;
-  
-       return handleError(error);  //Check for the exact error code to know what to return
-   }
+    } else
+    return handleError(error);  //Check for the exact error code to know what to return
+  }
+  else
+  return handleError(error);  //Check for the exact error code to know what to return
 };
 
 export const handleError =(error)=>{
@@ -63,7 +51,7 @@ export const handleError =(error)=>{
         if(error.response.status >=500 && error.response.status < 600){
             message = 'something went wrong, try again please.';
         }else {
-            message = error.response.data.message;
+            message = error.response.data.message || error.response.data.Message;
         }
         
     } else if (error.request) {
