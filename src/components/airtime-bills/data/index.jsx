e02818ca70a4b591bc7bd0f5b39c4466ef6ef4c5
damiadeh <damiadehjames@gmail.com>
2019-06-14 11:53:21 +0100

    [Dami] Get Data Beneficiary page done

--- src/components/airtime-bills/data/index.jsx
+++ src/components/airtime-bills/data/index.jsx
@@ -4,42 +4,46 @@ import airtelLogo from '../../../assets/img/phone-data.svg'
 
 
 const index = (props) => {
-    let  index = (<div className="col-sm-12">
-    <div className="row">
-        <div className="col-sm-12">
-            <div className="max-600 m-t-40">
-                <center>
-                    <img src={dataLogo} className="m-b-30" alt="Data Icon"/>
-                    <p className="grey-text no-paylink">No saved data purchase</p>
-                    <button className="btn-alat">Buy Data</button>
-                  </center>
-            </div>
-         </div>
-    </div>
- </div>);
-    if (props.hasbeneficiaries){
+    let index = (
+            <div className="col-sm-12">
+                <div className="max-600 m-t-40">
+                    <center>
+                        <img src={dataLogo} className="m-b-30" alt="Data Icon" />
+                        <p className="grey-text no-paylink">No saved data purchase</p>
+                        <button className="btn-alat">Buy Data</button>
+                    </center>
+                </div>
+            </div>);
+
+    let container = (
+                <div className="col-sm-12">
+                    <div className="row">
+                        {index}
+                    </div>
+                </div>);
+
+
+    if (props.hasbeneficiaries) {
         index = null;
-        props.beneficiaries.map((beneficiary, key) => (
-            index += (<div className="col-sm-12">
-        <div className="row">
-            <div className="col-sm-12 col-md-4">
-                  <div className="al-card airtime-card">
-                      <div className="clearfix">
-                          <div className="network-img">
-                              <img src={airtelLogo} alt="airtellogo" />
-                          </div>
-                          <div className="all-info">
-                              <p className="line-price">My MTN Line <span className="price">N5,000</span></p>
-                              <p className="num-ref">07032414095<span className="price"><a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a></span></p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              
-              </div></div>)
-        ));
-        
+            props.beneficiaries.map((beneficiary, key) => (
+                index += (
+                    <div className="col-sm-12 col-md-4">
+                        <div className="al-card airtime-card">
+                            <div className="clearfix">
+                                <div className="network-img">
+                                    <img src={airtelLogo} alt="airtellogo" />
+                                </div>
+                                <div className="all-info">
+                                    <p className="line-price">My MTN Line <span className="price">N5,000</span></p>
+                                    <p className="num-ref">07032414095<span className="price"><a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a></span></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>)
+            ));
+
     }
-    return index;
+    return container;
 };
 export default index;
+
