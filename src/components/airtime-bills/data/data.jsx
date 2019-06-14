2df41298d47f73d07527fc90c486f3dacffdd60c
damiadeh <damiadehjames@gmail.com>
2019-06-14 09:46:18 +0100

    [Dami] Get Data Beneficiary page done 90%

--- src/components/airtime-bills/data/data.jsx
+++ src/components/airtime-bills/data/data.jsx
@@ -2,11 +2,6 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Index from '.';
 import * as actions from '../../../redux/actions/dataActions/export';
-// import { from } from 'rxjs';
-// import { dispatch } from 'rxjs/internal/observable/pairs';
-
-// var currentSubComponent = "Index";
-// var nextComponent;
 
 class Data extends Component{
     constructor(props) {
@@ -20,19 +15,8 @@ class Data extends Component{
         this.props.fetchBeneficiaries(this.state.user.token);
     }
    render() {
-
-    // switch(currentSubComponent){
-    //     case ("Index") :
-    //     nextComponent = <Index />;
-    //     break;
-    //     case ("changed") :
-    //     nextComponent = <h1>changed</h1>;
-    //     break; 
-    //     default :
-    //     nextComponent = <Index />;
-    // }
        return(
-           <Index hasBeneficiaries={this.props.beneficiaries.length > 0}/>
+           <Index beneficiaries={this.props.beneficiaries} hasBeneficiaries={this.props.beneficiaries.length > 0}/>
        );
    }
 }
--- src/components/airtime-bills/data/index.jsx
+++ src/components/airtime-bills/data/index.jsx
@@ -1,5 +1,6 @@
 import React from 'React';
 import dataLogo from '../../../assets/img/phone-data.svg'
+import airtelLogo from '../../../assets/img/phone-data.svg'
 
 
 const index = (props) => {
@@ -16,8 +17,28 @@ const index = (props) => {
          </div>
     </div>
  </div>);
-    if (props.hasBeneficiaries){
-        index = <center>Has beneficiarie</center>;
+    if (props.hasbeneficiaries){
+        index = null;
+        props.beneficiaries.map((beneficiary, key) => (
+            index += (<div className="col-sm-12">
+        <div className="row">
+            <div className="col-sm-12 col-md-4">
+                  <div className="al-card airtime-card">
+                      <div className="clearfix">
+                          <div className="network-img">
+                              <img src={airtelLogo} alt="airtellogo" />
+                          </div>
+                          <div className="all-info">
+                              <p className="line-price">My MTN Line <span className="price">N5,000</span></p>
+                              <p className="num-ref">07032414095<span className="price"><a href="#"><i class="fa fa-trash-o" aria-hidden="true"></i></a></span></p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              
+              </div></div>)
+        ));
+        
     }
     return index;
 };
