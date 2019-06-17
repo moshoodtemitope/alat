dd3d47280c55c69a32de50c92a97f9a86b937e30
Damilare Adepoju <damiadehjames@gmail.com>
2019-06-17 10:21:20 +0100

    [Dami]Buy Data page ---

--- src/redux/reducers/data.reducer.js
+++ src/redux/reducers/data.reducer.js
@@ -2,32 +2,32 @@ import * as actionTypes from "../constants/dataConstants/data.constant";
 
 import {updateObject} from '../actions/dataActions/data.actions';
 
-const mock = [
-    {
-        BillerName: "MTN",
-        PhoneNumber: "08166445586",
-        Amount: 5000,
-        BillerAlias: "My Data"
-    },
-    {
-        BillerName: "GLO",
-        PhoneNumber: "08166000586",
-        Amount: 300,
-        BillerAlias: "WIfe Data"
-    },
-    {
-        BillerName: "9MOBILE",
-        PhoneNumber: "08099445586",
-        Amount: 80,
-        BillerAlias: "My Data"
-    },
-    {
-        BillerName: "AIRTEL",
-        PhoneNumber: "08099445586",
-        Amount: 80,
-        BillerAlias: "Airtel Data"
-    }
-];
+// const mock = [
+//     {
+//         BillerName: "MTN",
+//         PhoneNumber: "08166445586",
+//         Amount: 5000,
+//         BillerAlias: "My Data"
+//     },
+//     {
+//         BillerName: "GLO",
+//         PhoneNumber: "08166000586",
+//         Amount: 300,
+//         BillerAlias: "WIfe Data"
+//     },
+//     {
+//         BillerName: "9MOBILE",
+//         PhoneNumber: "08099445586",
+//         Amount: 80,
+//         BillerAlias: "My Data"
+//     },
+//     {
+//         BillerName: "AIRTEL",
+//         PhoneNumber: "08099445586",
+//         Amount: 80,
+//         BillerAlias: "Airtel Data"
+//     }
+// ];
 
 const initialState = {
     currentComponent : "Index",
@@ -40,8 +40,8 @@ const reducer = (state = initialState, action) => {
         case actionTypes.SWITCH_CURRENT_COMPONENT:
             return updateObject(state, {currentComponent: action.name});
         case actionTypes.FETCH_DATA_BENEFICIARIES_SUCCESS:
-            // return updateObject(state, {beneficiaries: action.data, isFetching : false});
-            return updateObject(state, {beneficiaries: mock, isFetching : false});
+            return updateObject(state, {beneficiaries: action.data, isFetching : false});
+            // return updateObject(state, {beneficiaries: mock, isFetching : false});
         case actionTypes.IS_FETCHING_TRUE:
            return updateObject(state, {isFetching: true});
         case actionTypes.IS_FETCHING_FALSE:
