961905912c565f6f1c0458f44ff6cce8d1fe7b50
Moshood Temitope <temitope.moshood@wemabank.com>
2019-06-29 13:14:20 +0100

    [Moshood] Pointed api link to QA

--- src/components/transfer/cash-transfer/transfer-otp.jsx
+++ src/components/transfer/cash-transfer/transfer-otp.jsx
@@ -136,24 +136,7 @@ class TransferOtp extends React.Component{
             let {OtpValue} = this.state;
         return(
             <Fragment>
-                <InnerContainer>
-                    <TransferContainer>
-                    <div className="row">
-                            <div className="col-sm-12">
-                                <p className="page-title">Send Money</p>
-                            </div>
-                            <div className="col-sm-12">
-                                <div className="tab-overflow">
-                                    <div className="sub-tab-nav">
-                                        <ul>
-                                            <li><a href="accounts.html" className="active">Bank Transfer</a></li>
-                                            <li><a href="statement.html">Send To Contacts</a></li>
-                                            <li><a href="#">Cardless Withdrawal</a></li>
-                                            <li><a href="#">FX Transfer</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
+                    
                             <div className="col-sm-12">
                                 <div className="row">
                                     <div className="col-sm-12">
@@ -225,9 +208,8 @@ class TransferOtp extends React.Component{
                                     </div>
                                 </div>
                             </div>
-                    </div>
-                    </TransferContainer>
-                </InnerContainer>
+                   
+                    
             </Fragment>
         )
     }
