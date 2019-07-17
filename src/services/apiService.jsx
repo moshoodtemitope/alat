b5b05e7a1b2723dd2c0be98c7cf181e7192973ae
Damilare <Damilare.Adepoju@wemabank.com>
2019-07-17 09:20:10 +0100

    [Dami]added bill icon to bills beneficiaries

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -96,7 +96,7 @@ export class ApiService {
                 // console.log("successful");
                 return service;
             }).catch(function (error) {
-               
+            
               if (error.response) {
                 // The request was made and the server responded with a status code
                 // that falls out of the range of 2xx
