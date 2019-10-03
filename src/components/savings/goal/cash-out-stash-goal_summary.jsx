a10a63b37ebb7836cf57a03423b93efa343c7298
Moshood Temitope <temitope.moshood@wemabank.com>
2019-10-03 16:22:41 +0100

    fixed back issues

--- src/components/savings/goal/cash-out-stash-goal_summary.jsx
+++ src/components/savings/goal/cash-out-stash-goal_summary.jsx
@@ -174,13 +174,14 @@ class CashoutStashGoal extends Component {
 
 
                                         </div>
-
-
-                                    </div>
-                                    <a style={{ cursor: "pointer" }} onClick={() => { this.props.dispatch(actions.ClearAction(customerGoalConstants.CUSTOMER_GOAL_REDUCER_CLEAR));
+                                        <a style={{ cursor: "pointer" }} onClick={() => { this.props.dispatch(actions.ClearAction(customerGoalConstants.CUSTOMER_GOAL_REDUCER_CLEAR));
                                                 this.props.history.push('/savings/choose-goal-plan') }} className="add-bene m-t-50">
                                                 Go back
                                         </a>
+
+
+                                    </div>
+                                   
 
 
 
